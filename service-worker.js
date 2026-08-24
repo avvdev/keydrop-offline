a532c539cc62d612f9724f6cfcf6e1712e40479e
avvdev <avvdev@users.noreply.github.com>
2026-08-24 10:18:49 -0700

    Publish Keydrop Offline compatibility test

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const CACHE = "keydrop-offline-spike-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./decrypt.bundle.js",
+  "./manifest.webmanifest",
+  "./icon.svg",
+  "./smoke-not-a-secret.toml.enc",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((response) => response || Response.error()));
+});
